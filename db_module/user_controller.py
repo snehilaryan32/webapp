50978734839f3a7232e96824ad07e6592bca9c55
snehilaryan32 <aryan.s@northeastern.edu>
2024-02-06 23:30:41 -0500

    Update user model and controller

--- db_module/user_controller.py
+++ db_module/user_controller.py
@@ -1,36 +1,34 @@
 from . import db_conn
 from models.user import User, Base
 from sqlalchemy.orm import sessionmaker
-from psycopg2.errors import UniqueViolation
 from sqlalchemy.exc import IntegrityError
 import datetime
 
 
 #################################################User Creation############################################
-def create_user(email, first_name, last_name, password):
+def create_user(user_details):
     engine = db_conn.db_engine()
     status_code = None
-    message = None
-
+    description = None
     if engine:
         try:
             Session = sessionmaker(bind=db_conn.db_engine())
             session = Session()
-            new_user = User(username=email, first_name=first_name, last_name=last_name, password=password)
+            new_user = User(user_details)
             session.add(new_user)
             session.commit()
             session.close()
-            status_code = 200
-            message = "User Created"
+            status_code = 201
+            # description = "User Created"
         except IntegrityError:
             status_code = 400
-            message = "User already exists"
+            description = "User already exists"
         except Exception as e:   
-            status_code = 500   
-            message = str(e)
-        return {"status_code": status_code, "message": message}
+            status_code = 500
+            description = str(e)
+        return {"status_code": status_code, "description": description}
     else:
-        return {"status_code": 500, "message": "Internal Server Error"}
+        return {"status_code": 500, "description": "Internal Server Error"}
 
 #################################################Get User Details#########################################
 def get_user_details(username):
@@ -55,30 +53,23 @@ def get_hashed_password(username):
 def update_user_details(username, updated_user_details):
     engine = db_conn.db_engine()
     status_code = None
-    message = None
+    # message = None
     if engine:
         Session = sessionmaker(bind=db_conn.db_engine())
         session = Session()
-        try:
-            user = session.query(User).filter_by(username=username).first()
-            user.first_name = updated_user_details['first_name']
-            user.last_name = updated_user_details['last_name']
-            user.username = updated_user_details['email']
-            user.password = updated_user_details['password']
-            session.commit()
-            session.close()
-            status_code = 200
-            message = "User Updated"
-        except IntegrityError:
-            status_code = 400, 
-            message = "Updated email already exists"   
-        status_code = 500
-        message = "Internal Server Error"
+        user = session.query(User).filter_by(username=username).first()
+        user.first_name = updated_user_details['first_name']
+        user.last_name = updated_user_details['last_name']
+        user.password = updated_user_details['password']
+        user.account_updated = datetime.datetime.now()
+        session.commit()
+        session.close()
+        status_code = 204
+        #message = "User Updated"   
     else:
         status_code = 503
-        message = "Unable to connect to database"
         
-    return {"status_code": status_code, "message": message}
+    return {"status_code": status_code}
 
 
 
