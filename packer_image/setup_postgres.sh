660e12ab08d3fe5fbc0d9372b9e70b6312bad986
snehilaryan32 <aryan.s@northeastern.edu>
2024-02-20 16:30:46 -0500

    packer fmt

--- packer_image/setup_postgres.sh
+++ packer_image/setup_postgres.sh
@@ -2,9 +2,11 @@
 sudo postgresql-setup initdb
 PG_HBA_CONF="/var/lib/pgsql/data/pg_hba.conf"
 cp $PG_HBA_CONF ${PG_HBA_CONF}.backup
-sed -i 's/ident/md5/g' $PG_HBA_CONF
+sudo sed -i 's/ident/md5/g' $PG_HBA_CONF
+# sudo sed -i 's/peer/md5/g' $PG_HBA_CONF
 cd /tmp 
-sudo service postgresql start
+sudo systemctl enable postgresql
+sudo systemctl start postgresql
 sudo -u postgres psql -c "CREATE DATABASE $DB_NAME;"
 sudo -u postgres psql -c "CREATE USER $DB_USER WITH PASSWORD '$DB_PASS';"
 sudo -u postgres psql -c "ALTER USER $DB_USER WITH SUPERUSER;"
