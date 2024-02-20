89bf601663c97aaf0486dcd51a324746325e51d8
snehilaryan32 <aryan.s@northeastern.edu>
2024-02-20 05:03:34 -0500

    Added pg conh update with md5

--- packer_image/setup_postgres.sh
+++ packer_image/setup_postgres.sh
@@ -1,5 +1,8 @@
 # Setup Postgres
 sudo postgresql-setup initdb
+PG_HBA_CONF="/var/lib/pgsql/data/pg_hba.conf"
+cp $PG_HBA_CONF ${PG_HBA_CONF}.backup
+sed -i 's/ident/md5/g' $PG_HBA_CONF
 cd /tmp 
 sudo service postgresql start
 sudo -u postgres psql -c "CREATE DATABASE $DB_NAME;"
