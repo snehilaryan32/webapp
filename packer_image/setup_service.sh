d0430e2fb70f6a14958e1a8af902aa2ca1338828
snehilaryan32 <aryan.s@northeastern.edu>
2024-02-20 22:30:32 -0500

    Added nologin system user

--- packer_image/setup_service.sh
+++ packer_image/setup_service.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# Move the service file, update selinux policy for the file, reload systemd manager configuration, and enable the service
+# Move the service file, update selinux policy for the file, reload systemd manager configuration, and enable the service, create a svcuser
 sudo mv /home/packer/flaskapp.service /etc/systemd/system/flaskapp.service
+sudo adduser csye6225 --system --shell /usr/sbin/nologin
 echo "SELINUX=permissive" | sudo tee /etc/selinux/config
 sudo systemctl daemon-reload
 sudo setenforce 0
