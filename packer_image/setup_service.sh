a278ca17935e2b1fde4c3933f464855d654985eb
snehilaryan32 <aryan.s@northeastern.edu>
2024-02-20 23:32:08 -0500

    Fixed the order of chown command

--- packer_image/setup_service.sh
+++ packer_image/setup_service.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Move the service file, update selinux policy for the file, reload systemd manager configuration, and enable the service, create a svcuser
 sudo mv /home/packer/flaskapp.service /etc/systemd/system/flaskapp.service
-sudo chown -R csye6225:csye6225 /home/packer/flask-app # Change the ownership of the directory to the user in svc file
 sudo adduser csye6225 --system --shell /usr/sbin/nologin
+sudo chown csye6225:csye6225 /etc/systemd/system/flaskapp.service
 echo "SELINUX=permissive" | sudo tee /etc/selinux/config
 sudo systemctl daemon-l start flaskappreload
 sudo setenforce 0
