af37420c6c944283d8c09f9a5b5a357f3704677b
snehilaryan32 <aryan.s@northeastern.edu>
2024-02-20 23:09:55 -0500

    removed env variables in the sh file

--- packer_image/setup_flask_app.sh
+++ packer_image/setup_flask_app.sh
@@ -4,10 +4,5 @@ cd flask-app
 # Set Up Python Environment after scp the new requirements 
 python3 -m venv myenv
 source myenv/bin/activate
-export DB_HOST="localhost"
-export DB_PORT=5432
-export DB_NAME=$DB_NAME
-export DB_USER=$DB_USER
-export DB_PASSWORD=$DB_PASS
 pip3 install --upgrade pip
 pip3 install -r requirements.txt
