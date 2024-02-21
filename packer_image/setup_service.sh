7014fb70251205c76d4e0f4b0daabd1951eb0ae3
snehilaryan32 <aryan.s@northeastern.edu>
2024-02-21 11:20:14 -0500

    Removed system user for testing

--- packer_image/setup_service.sh
+++ packer_image/setup_service.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 # Move the service file, update selinux policy for the file, reload systemd manager configuration, and enable the service, create a svcuser
 sudo mv /home/packer/flaskapp.service /etc/systemd/system/flaskapp.service
-sudo adduser csye6225 --system --shell /usr/sbin/nologin
-sudo chown csye6225:csye6225 /etc/systemd/system/flaskapp.service
+# sudo adduser csye6225 --system --shell /usr/sbin/nologin
+# sudo chown csye6225:csye6225 /etc/systemd/system/flaskapp.service
+# sudo chmod 770 /home/packer/flask-app/myenv/bin/gunicorn
+
 echo "SELINUX=permissive" | sudo tee /etc/selinux/config
-sudo systemctl daemon-l start flaskappreload
+sudo systemctl daemon-l 
 sudo setenforce 0
 sudo systemctl enable flaskapp
 sudo systemctl restart flaskapp
