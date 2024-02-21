67e71d0d97b21dcb5dadd3825b07f1022c4d7047
snehilaryan32 <aryan.s@northeastern.edu>
2024-02-20 20:57:04 -0500

    Updated commands in shell for selinux

--- packer_image/setup_service.sh
+++ packer_image/setup_service.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Move the service file, update selinux policy for the file, reload systemd manager configuration, and enable the service
 sudo mv /home/packer/flaskapp.service /etc/systemd/system/flaskapp.service
-sudo setenforce 0
+echo "SELINUX=permissive" | sudo tee /etc/selinux/config
 sudo systemctl daemon-reload
 sudo systemctl enable flaskapp
 sudo systemctl start flaskapp
