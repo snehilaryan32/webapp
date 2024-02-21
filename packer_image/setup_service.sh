7fabd770dc5c3ed34819117d93e971e24e16ed2c
snehilaryan32 <aryan.s@northeastern.edu>
2024-02-21 12:18:26 -0500

    Updated the service file to use ssh user

--- packer_image/setup_service.sh
+++ packer_image/setup_service.sh
@@ -2,9 +2,9 @@
 # Move the service file, update selinux policy for the file, reload systemd manager configuration, and enable the service, create a svcuser
 sudo mv /home/packer/flaskapp.service /etc/systemd/system/flaskapp.service
 sudo adduser csye6225 --system --shell /usr/sbin/nologin
-sudo chown csye6225:csye6225 /home/packer/flask-app
-sudo chown csye6225:csye6225 /home/packer/flask-app/myenv
-sudo chown csye6225 /home/packer/flask-app/myenv/bin/gunicorn
+# sudo chown csye6225:csye6225 /home/packer/flask-app
+# sudo chown csye6225:csye6225 /home/packer/flask-app/myenv
+# sudo chown csye6225 /home/packer/flask-app/myenv/bin/gunicorn
 
 echo "SELINUX=permissive" | sudo tee /etc/selinux/config
 sudo systemctl daemon-reload
