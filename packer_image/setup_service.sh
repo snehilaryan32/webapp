105e5b2099651dd5b9216288dee5e800313f0d6d
snehilaryan32 <aryan.s@northeastern.edu>
2024-02-21 14:43:28 -0500

    Updated permissions for csye6225

--- packer_image/setup_service.sh
+++ packer_image/setup_service.sh
@@ -5,11 +5,14 @@ sudo groupadd csye6225
 sudo useradd -r -g csye6225 -s /usr/sbin/nologin csye6225
 sudo chown -R csye6225:csye6225 /home/packer/flask-app
 sudo chmod -R 755 /home/packer/flask-app
-# sudo chown csye6225:csye6225 /home/packer/flask-app/myenv
-# sudo chown csye6225 /home/packer/flask-app/myenv/bin/gunicorn
+sudo chown csye6225:csye6225 /home/packer/flask-app/myenv/bin/gunicorn
+sudo chmod 755 /home/packer/flask-app/myenv/bin/gunicorn
+sudo chown csye6225:csye6225 /home/packer/flaskapp.env
+sudo chmod 644 /home/packer/flaskapp.env
+sudo chmod o+x /home/packer
 echo "SELINUX=permissive" | sudo tee /etc/selinux/config
 sudo systemctl daemon-reload
 sudo setenforce 0
 sudo systemctl enable flaskapp
 sudo systemctl start flaskapp
-# sudo systemctl status flaskapp
+sudo systemctl status flaskapp
