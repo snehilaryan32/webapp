14c5c0dda3eb946446029cc59b819aad8f8afc89
snehilaryan32 <aryan.s@northeastern.edu>
2024-02-21 13:54:36 -0500

    Removed extra chown

--- packer_image/setup_service.sh
+++ packer_image/setup_service.sh
@@ -4,8 +4,8 @@ sudo mv /home/packer/flaskapp.service /etc/systemd/system/flaskapp.service
 sudo groupadd csye6225
 sudo useradd -r -g csye6225 -s /usr/sbin/nologin csye6225
 sudo chown -R csye6225:csye6225 /home/packer/flask-app
-sudo chown csye6225:csye6225 /home/packer/flask-app/myenv
-sudo chown csye6225 /home/packer/flask-app/myenv/bin/gunicorn
+# sudo chown csye6225:csye6225 /home/packer/flask-app/myenv
+# sudo chown csye6225 /home/packer/flask-app/myenv/bin/gunicorn
 echo "SELINUX=permissive" | sudo tee /etc/selinux/config
 sudo systemctl daemon-reload
 sudo setenforce 0
